8635e41a34b7344cf4c71366f2347476228d198c
vedaapm <vedam407@gmail.com>
2024-05-16 15:19:02 +0530

    OJT1

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import { Card, Grid, Link, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import Copyright from "./Copyright";
+
+const Footer = () => {
+  return (
+    <Box pb={3}>
+      
+    </Box>
+  );
+};
+
+export default Footer;
